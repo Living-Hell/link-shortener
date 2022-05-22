6fbd906516ab252c45c1a9c5fb55f5828d1db7a8
Living-Hell <s.living.hell@gmail.com>
2022-05-23 00:52:13 +0530

    Code for the application completed (but issue in LinkResult.js file)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import BackgroundAnimation from './BackgroundAnimation';
+import InputShortener from './InputShortener';
+import LinkResult from './LinkResult';
+
+
 
 function App() {
+  const [inputvalue, setInputValue] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <InputShortener setInputValue={setInputValue}/>
+      <BackgroundAnimation/>
+      <LinkResult inputvalue={inputvalue}/>
     </div>
   );
 }
---
+++ src/BackgroundAnimation.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function BackgroundAnimation() {
+  return (
+    <div className="background">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+  )
+}
